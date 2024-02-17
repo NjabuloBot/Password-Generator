14bb56f2b6f102fd0109edd82397417f8214085e
Njabulo <nnshiwu@gmail.com>
2024-02-17 17:48:15 +0200

    Imported the random module.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import string
+import random
 
 #Define the possible characters for the password
 letters = string.ascii_letters
 digits = string.digits
 symbols = string.punctuation
 
+#Combine all characters
 all_characters = letters + digits + symbols
