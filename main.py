735dab87f0a485e50637d72a0cd1cb791f787038
Njabulo <nnshiwu@gmail.com>
2024-02-17 17:44:46 +0200

    Declared a variable named all_characters and assigned it the result of concatenating your existing variables.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import string
 
+#Define the possible characters for the password
 letters = string.ascii_letters
 digits = string.digits
 symbols = string.punctuation
+
+all_characters = letters + digits + symbols
