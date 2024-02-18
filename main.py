6f66c31c6d55102daa6e867cb41778163a711ffb
Njabulo <nnshiwu@gmail.com>
2024-02-18 08:53:59 +0200

    Done with password generator using Regular expression.

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def generate_password(length=16,nums=1,special_chars=1,uppercase=1,lowercase=1):
       constraints = [(nums,r"\d"),(lowercase,r'[a-z]'),(uppercase,r'[A-Z]'),(special_chars,fr'[{symbols}]')]
       #Check constraints
       if all(constraint <= len(re.findall(pattern, password))
-        for constraint, pattern in constraints): 
-         break;
-      return password
+             for constraint, pattern in constraints):
+          break;
+   return password
 
-new_password = generate_password(length=8, nums=1, special_chars=1, uppercase=1, lowercase=1)
-print(f'\n{new_password}')
+if __name__ =="__main__":
+   new_password = generate_password()
+   print(f'\nGenerated password:{new_password}')
