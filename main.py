9a97cc71ff7be1efdef7dde50ec953b8ecfcaeae
Njabulo <nnshiwu@gmail.com>
2024-02-18 08:20:56 +0200

    Added a for loop.

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def generate_password(length,nums,special_chars,uppercase,lowercase):
       #Generate password
       for _ in range(length):
          password+=secrets.choice(all_characters)
-      constraints = [(nums,r"[0-9]"),(lowercase,r'[a-z]'),(uppercase,r'[A-Z]'),(special_chars,r'')]
+      constraints = [(nums,r"\d"),(lowercase,r'[a-z]'),(uppercase,r'[A-Z]'),(special_chars,fr'[{symbols}]')]
+      #Check constraints
+      for constraint,pattern in constraints:
+         pass
       return password
 
 # new_password = generate_password(8)
 # print(f'\n{new_password}')
-pattern = r'\.'
-quote = 'Not all those who wander are lost.'
-print(re.findall(pattern,quote))
